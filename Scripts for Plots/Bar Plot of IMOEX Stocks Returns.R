fc085e987347fe8faa60de6ca8a453356bc619ce
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-20 11:20:38 +0500

    Update Bar Plot of IMOEX Stocks Returns.R
    Added IMOEX return as number on legend and a horizontal line on plot

--- Scripts for Plots/Bar Plot of IMOEX Stocks Returns.R
+++ Scripts for Plots/Bar Plot of IMOEX Stocks Returns.R
@@ -30,6 +30,25 @@ bar.plt.imoex <- function(x){ # Bar Plot with IMOEX Stocks Returns
   
   p.seq <- seq(from = ceiling(min(l) * -1) * -1, to = ceiling(max(l)), by = 1)
   
+  p <- read_html("https://smart-lab.ru/q/shares/") # Get info from website
+  
+  div <- p %>% html_nodes('table') %>% .[[1]] -> tab # Table
+  
+  f <- tab %>% html_nodes('tr') #
+  
+  L <- NULL # Reorganise data into data frame
+  
+  for (n in 2:length(f)){ d <- f[n] %>% html_nodes('td') %>% html_text()
+  
+    P <- gsub('["\n"]', '', gsub('["\t"]', '', d[8]))
+    
+    P <- as.character(read.fwf(textConnection(P), widths = c(nchar(P) - 1, 1),
+                               colClasses = "character")[1])
+    
+    if (isTRUE(grepl("\\+", P))){ P <- as.numeric(gsub("\\+", "", P)) }
+  
+  L <- rbind.data.frame(L, cbind(d[3], d[7], P)) } # ticker, price & %
+  
   plt <- barplot(l,names.arg=names(l),main="IMOEX Stocks Returns for Today (%)", 
                  col = c(rep("green4", length(l) - sum(l < 0)),
                          rep("red3", sum(l < 0))), horiz = F, las = 2,
@@ -40,22 +59,24 @@ bar.plt.imoex <- function(x){ # Bar Plot with IMOEX Stocks Returns
   abline(h = p.seq[-match(0, p.seq)],col="grey",lty=3) # Horizontal grey lines
   abline(h = mean(l), col = "blue", lwd = 3) # Mean line
   abline(h = median(l), col = "green", lwd = 3) # Median line
+  abline(h = L[1,3], col = "purple", lwd = 3) # IMOEX
   
   legend(x = "bottom", inset = c(0, -.25), cex = .85, bty = "n", horiz = T,
          legend = c((sprintf("Mean: %s %%", round(mean(l), 2))),
-                    sprintf("Median: %s %%", round(median(l), 2))),
-         col = c("blue", "green"), xpd = T, pch = 15)
+                    sprintf("Median: %s %%", round(median(l), 2)),
+                    sprintf("%s: %s %%", L[1,1], L[1,3])),
+         col = c("blue", "green", "purple"), xpd = T, pch = 15)
   
   m <- round(min(l) * -1 + max(l),0)/10^(nchar(round(min(l) * -1 + max(l),0)))
   
-  if (m > 0 && m < 1){ mn <- 1 * 10 ^ (nchar(m) - 3) }
+  if (m > 0 && m < 1){ mn <- 1 * 10 ^ (nchar(m) - 4) }
   
-  else if (m > 1 && m < 2){ mn <- 2 * 10 ^ (nchar(m) - 3) }
+  else if (m > 1 && m < 2){ mn <- 2 * 10 ^ (nchar(m) - 4) }
   
-  else if (m > 2 && m < 5){ mn <- 5 * 10 ^ (nchar(m) - 3) }
+  else if (m > 2 && m < 5){ mn <- 5 * 10 ^ (nchar(m) - 4) }
   
-  axis(side = 2, las = 1, at = seq(-99, 99, mn * 2)) # Axes
-  axis(side = 4, las = 1, at = seq(-100, 100, mn)) # Axes
+  axis(side = 2, las = 1, at = p.seq, labels = p.seq) # Axes
+  axis(side = 4, las = 1, at = p.seq, labels = p.seq) # Axes
   
   par(mar = c(8, 4, 3, 4)) # Define borders of the plot
   
