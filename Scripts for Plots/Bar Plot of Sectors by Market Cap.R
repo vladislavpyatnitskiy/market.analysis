cd4996070331753a2631cba4efab433690c8587a
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-11-26 11:15:49 +0500

    Update Bar Plot of Sectors by Market Cap.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence for initial data frame
    • Reduced number of colours available

--- Scripts for Plots/Bar Plot of Sectors by Market Cap.R
+++ Scripts for Plots/Bar Plot of Sectors by Market Cap.R
@@ -2,17 +2,12 @@ library("rvest") # Library
 
 finviz.sectors.marketcap <- function(x){ # Data Frame with info about sectors
   
-  s <- read_html(sprintf("https://finviz.com/%s", x)) # HTML 
+  y <- read_html(sprintf("https://finviz.com/%s", x)) %>%
+    html_nodes('table') %>% .[[8]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text()
   
-  tab <- s %>% html_nodes('table') %>% .[[8]] # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  d <- NULL # Data Frame with Market Cap values
-  
-  for (n in 0:(length(y)/15)){ d <- rbind(d, cbind(y[(2+n*15)], y[(3+n*15)])) }
-  
-  d <- d[-nrow(d),] # Reduce last column
+  d <- data.frame(y[seq(from = 2, to = length(y), by = 15)],
+                  y[seq(from = 3, to = length(y), by = 15)])
   
   rownames(d) <- d[,1] # Assign row names
   
@@ -23,8 +18,7 @@ finviz.sectors.marketcap <- function(x){ # Data Frame with info about sectors
   for (n in 1:nrow(d)){ d[n,1] <- read.fwf(textConnection(d[n,1]),
                                            widths = c(nchar(d[n,1]) - 1, 1),
                                            colClasses = "character")[,1] }
-  
-  d <- as.data.frame(d) # Transform to data frame
+  d <- as.data.frame(d) # Data frame
   
   for (n in 1:ncol(d)){ d[,n] <- as.numeric(d[,n]) } # Make data numeric
   
@@ -43,19 +37,15 @@ finviz.sectors.marketcap <- function(x){ # Data Frame with info about sectors
     10 ^ (nchar(round(min(d))) - 1) # Round maximum value up
   
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
-        "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
-        "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
-        "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
-        "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
-        "#895c8b","#bd5975")
+        "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a")
   
-  B <- barplot(d, names.arg=names(d), horiz=T, las=1, xlim=c(mn, mx), xpd=F,
+  B <- barplot(d,names.arg=names(d),horiz=T,las=1,xlim=c(mn, max(d)+1), xpd=F,
                col=C, main = "Sectors by Market Capitalisation in $Trillions")
   
   p.seq <- seq(round(min(d) - 1), mx, by = 1) # Values for axes
   
-  for (n in p.seq){ abline(v = n, col ="grey",lty = 3) } # Put horiz lines
-  abline(h = B, col ="grey",lty = 3) # Put vertical lines
+  for (n in p.seq){ abline(v = n, col ="grey", lty = 3) } # Put horiz lines
+  abline(h = B, col = "grey", lty = 3) # Put vertical lines
   abline(v = mean(d), col = "red", lwd = 3) # Mean percentage line
   abline(v = median(d), col = "green", lwd = 3) # Median percentage line
   
