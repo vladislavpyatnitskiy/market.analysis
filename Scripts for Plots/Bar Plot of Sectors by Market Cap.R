b659a016cfe262c9baa38f159346a3d54aba24f4
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-03-11 10:35:02 +0500

    Update Bar Plot of Sectors by Market Cap.R
    Optimised script:
    
    • Reduced excessive lines
    • Rewrote part for grid
    • Rewrote part for Mean, Median and colours

--- Scripts for Plots/Bar Plot of Sectors by Market Cap.R
+++ Scripts for Plots/Bar Plot of Sectors by Market Cap.R
@@ -30,32 +30,23 @@ finviz.sectors.marketcap <- function(x){ # Data Frame with info about sectors
   
   d <- sort(d, decreasing = F) # Sort 
   
-  mx <- ceiling(round(max(d)) / 10 ^ (nchar(round(max(d))) - 1)) *
-    10 ^ (nchar(round(max(d))) - 1) # Round maximum value up
-  
-  mn <- trunc(round(min(d)) / 10 ^ (nchar(round(min(d))) - 1)) *
-    10 ^ (nchar(round(min(d))) - 1) # Round maximum value up
-  
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
         "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a")
   
-  B <- barplot(d,names.arg=names(d),horiz=T,las=1,xlim=c(mn, max(d)+1), xpd=F,
-               col=C, main = "Sectors by Market Capitalisation in $Trillions")
+  B <- barplot(d,names.arg=names(d),horiz=T,las=1,xlim=c(0, max(d) + 1), xpd=F,
+               col=C, main="US Sectors by Market Capitalisation in $Trillions")
   
-  p.seq <- seq(round(min(d) - 1), mx, by = 1) # Values for axes
+  grid(nx = NULL, ny = 1, col = "grey", lwd = 1) # Vertical lines
+  abline(h = B, col = "grey", lty = 3) # Horizontal lines
   
-  for (n in p.seq){ abline(v = n, col ="grey", lty = 3) } # Put horiz lines
-  abline(h = B, col = "grey", lty = 3) # Put vertical lines
-  abline(v = mean(d), col = "red", lwd = 3) # Mean percentage line
-  abline(v = median(d), col = "green", lwd = 3) # Median percentage line
-  
-  axis(side = 1, at = p.seq) # Assign values for horizontal axis
+  vals = list(list(mean(d), median(d)), c("red", "green")) # Mean & Median 
+  for (n in 1:2){ abline(v = vals[[1]][[n]], col = vals[[2]][n], lwd = 3) }
   
   par(mar = c(4, 11, 4, 4)) # Define borders of the plot
   
-  legend(x="bottom",inset=c(0,-.17),cex=.9,bty="n",horiz=T,col=c("red","green"),
-         legend=c((sprintf("Mean: %s",round(mean(d),2))),
-                  sprintf("Median: %s",round(median(d),2))),xpd=T,pch=15)
+  legend(x="bottom", inset=c(0,-.22), cex=.9, bty="n", horiz=T, col=vals[[2]],
+         legend=c((sprintf("Mean: %s",round(mean(d), 2))),
+                  sprintf("Median: %s",round(median(d), 2))), xpd=T, pch=15)
   
   box() # Make borders for plot
 }
