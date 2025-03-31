050a3a5142d5124f9bbc4df18f794b868fd49122
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-03-31 12:49:00 +0500

    Update Bar Plot of Sectors by Dividend Yield.R
    Optimised script:
    
    • Reduced excessive lines
    • Rewrote part for grid appearance

--- Scripts for Plots/Bar Plot of Sectors by Dividend Yield.R
+++ Scripts for Plots/Bar Plot of Sectors by Dividend Yield.R
@@ -15,11 +15,10 @@ finviz.sectors.dividends.bar <- function(x){ # Bar Plot with Dividend Yields
   
   colnames(d) <- c("Dividend Yield (%)") # Column name
   
-  for (n in 1:nrow(d)){ # Reduce "%" from Market Cap column values
-    
-    d[n,1] <- read.fwf(textConnection(d[n,1]), widths = c(nchar(d[n,1]) - 1, 1),
-                       colClasses = "character")[,1] }
-  
+  for (n in 1:nrow(d)){ d[n,1] <- read.fwf(textConnection(d[n,1]),
+                                           widths = c(nchar(d[n,1]) - 1, 1),
+                                           colClasses = "character")[,1]
+  }
   d <- as.data.frame(d) # Transform to data frame
   
   for (n in 1:ncol(d)){ d[,n] <- as.numeric(d[,n]) } # Make data numeric
@@ -32,28 +31,18 @@ finviz.sectors.dividends.bar <- function(x){ # Bar Plot with Dividend Yields
   
   d <- sort(d, decreasing = F) # Sort in a descending way
   
-  mx <- ceiling(round(max(d)) / 10 ^ (nchar(round(max(d))) - 1)) *
-    10 ^ (nchar(round(max(d))) - 1) # Round maximum value up
-  
-  mn <- trunc(round(min(d)) / 10 ^ (nchar(round(min(d))) - 1)) *
-    10 ^ (nchar(round(min(d))) - 1) # Round maximum value up
-  
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
         "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a")
   
-  B <- barplot(d, names.arg = names(d), horiz = T, las = 1, xlim = c(0, mx + 1),
+  B <- barplot(d, names.arg = names(d), horiz=T, las=1, xlim = c(0, max(d)+1),
                xpd = F, col = C, main = "Sectors by Dividend Yield (%)")
   
-  p.seq <- seq(0, mx + 1, by = 0.5) # Values for axes
-  
-  for (n in p.seq){ abline(v = n, col ="grey",lty = 3) } # Put horiz lines
-  abline(h = B, col ="grey",lty = 3) # Put vertical lines
+  grid(nx = NULL, ny = 1, lty = 3, col = "grey") # Horizontal lines
+  abline(h = B, col ="grey",lty = 3) # Vertical lines
   abline(v = mean(d), col = "red", lwd = 3) # Mean percentage line
   abline(v = median(d), col = "green", lwd = 3) # Median percentage line
   
-  axis(side = 1, at = seq(from = .5, to = mx + 1, by = 1))
-  
-  par(mar = c(4, 11, 4, 4)) # Define borders of the plot
+  par(mar = c(4, 11, 6, 4)) # Define borders of the plot
   
   legend(x = "bottom", inset = c(0, -.19), cex = .85, bty = "n", horiz = T,
          legend = c((sprintf("Mean: %s", round(mean(d), 2))),
