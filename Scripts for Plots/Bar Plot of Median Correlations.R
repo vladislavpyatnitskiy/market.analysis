afc2f90887c2b4dec1aa45e6d3edabc7014b8dfd
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-08-22 11:19:51 +0500

    Create Bar Plot of Median Correlations.R
    Added script getting median correlation values for each sector

---
+++ Scripts for Plots/Bar Plot of Median Correlations.R
@@ -0,0 +1,80 @@
+lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libraries
+
+bar.plt.sector.cor <- function(data=T){
+  
+  x <- c("XLC","XLY","XLP","XLE","XLF","XLV","XLI","XLK","XLB","XLRE","XLU")
+  
+  y <- c("Communications", "Consumer Disc.", "Consumer Staples",
+         "Energy", "Financials", "Health Care", "Industrials", "IT",
+         "Materials", "Real Estate", "Utilities")
+  
+  p <- NULL # Create an empty variable
+  
+  for (a in x) p <- cbind(p, getSymbols(a, src = "yahoo", auto.assign = F)[,4])
+  
+  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Eliminate NAs
+  
+  colnames(p) <- y
+  
+  M <- cor(as.matrix(diff(log(as.timeSeries(p)))[-1,])) # Correlation matrix
+  
+  # Extract unique pairs and their correlations
+  C <- which(upper.tri(M, diag = T), arr.ind = T)
+  
+  # Put them into one data frame
+  unique_pairs <- data.frame(Variable1 = rownames(M)[C[, 1]],
+                             Variable2 = rownames(M)[C[, 2]],
+                             Correlation = M[C]
+  )
+  # Filter out pairs with correlation equal to 1
+  f <- unique_pairs[unique_pairs$Correlation != 1, ]
+  
+  rownames(f) <- seq(nrow(f)) # Put ascending numbers into row names
+  
+  colnames(f) <- c("Security 1", "Security 2", "Correlation") # Column names
+  
+  cor.names <- unique(c(f[,1], f[,2])) # Show only unqiue tickers
+  
+  l <- NULL # Calculate median and mean correlation for each security
+  
+  for (n in 1:length(cor.names)){ k <- cor.names[n] # Select ticker
+  
+  v <- f[f$`Security 1` == k | f$`Security 2` == k, ] # ticker's correlations
+  
+  l <- rbind.data.frame(l, cbind(median(v[,3]), mean(v[,3]))) } # Join
+  
+  rownames(l) <- cor.names
+  colnames(l) <- c("Median", "Average") # Column names
+  
+  l <- l[order(l$Median), ] # Sort in an ascending way
+  
+  C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
+        "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
+        "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
+        "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
+        "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
+        "#895c8b","#bd5975") # Add colour range & Create barplot
+  
+  B <- barplot(
+    l[,1],
+    names.arg = rownames(l),
+    horiz = F,
+    col = C,
+    ylim = c(0, ceiling(max(l[,1]))),
+    main = "Median Correlations of US Sectors",
+    ylab = "Median Correlation Level",
+    las = 2
+    )
+  
+  p.seq <- seq(0, .95, .05)
+  axis(side = 2, at = p.seq, las = 1, labels = p.seq)
+  axis(side = 4, at = seq(0, 1, .05), las = 1, labels = seq(0, 1, .05))
+  
+  for (n in p.seq){ abline(h = n, col ="grey", lty = 3) } # Horizontal lines
+  abline(v = B, col = "grey", lty = 3) # Vertical lines
+  
+  par(mar = c(10, 5, 5, 5)) # Define borders of the plot
+  
+  box() # Add box
+}
+bar.plt.sector.cor(T)
