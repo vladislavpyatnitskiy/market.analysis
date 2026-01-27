827a5f4cf7a51982b97fbcbbe43e84f7f210cf07
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-27 11:01:29 +0500

    Enhance plotting and data download messages

--- Scripts for Plots/Line Plot for Indices.R
+++ Scripts for Plots/Line Plot for Indices.R
@@ -13,7 +13,15 @@ indices.plt <- function(us, rus, s=NULL, e=NULL){
     if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
     return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
   }
-  for (A in us){ U <- cbind(U, getData1(A, s, e)[,4]) } # Join data
+  for (A in us){ U <- cbind(U, getData1(A, s, e)[,4]) 
+  
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        A, which(us == A), length(us) + length(rus)
+      )
+    )
+  } # Join data
   
   getData2 <- function(A, s, e) { 
     if (is.null(s) && is.null(e))
@@ -23,6 +31,13 @@ indices.plt <- function(us, rus, s=NULL, e=NULL){
     return(get_candles(A, from = s, till = e, interval = 'daily')) 
   }
   for (A in rus){ D <- as.data.frame(getData2(A, s, e)[,c(3,8)])
+      
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        A, length(us) + which(rus == A), length(us) + length(rus)
+      )
+    )
   
     D <- D[!duplicated(D),] # Remove duplicates
     
@@ -39,10 +54,17 @@ indices.plt <- function(us, rus, s=NULL, e=NULL){
   
   par(mar = c(8, 2.5, 4, 2.5)) # Define borders of the plot
   
-  plot(DF[,1], ylim = c(min(DF), max(DF)),
-       lty = 1, type = "l", lwd = 2,
-       las = 1, xlab = "Trading Days", ylab = "Returns (%)",
-       main = "Performance of Major Benchmarks")
+  plot(
+    DF[,1], 
+    ylim = c(min(DF), max(DF)),
+    lty = 1, 
+    type = "l", 
+    lwd = 2,
+    las = 1, 
+    xlab = "Trading Days", 
+    ylab = "Returns (%)",
+    main = "Performance of Major Benchmarks"
+    )
   
   axis(side = 4, las = 2) # Right Y-Axis Values
   
@@ -52,8 +74,17 @@ indices.plt <- function(us, rus, s=NULL, e=NULL){
   
   for (n in 2:(ncol(DF))){ lines(DF[,n], col = n, lwd = 2) } # Plot indices
   
-  legend(x = "bottom", inset = c(0, -0.27), legend = colnames(DF), xpd = T,
-         col = seq(ncol(DF)), lwd = 2, cex = .5, bty = "n", horiz = T)
+  legend(
+    x = "bottom", 
+    inset = c(0, -0.2), 
+    legend = colnames(DF), 
+    xpd = T,
+    col = seq(ncol(DF)), 
+    lwd = 3, 
+    cex = .75, 
+    bty = "n", 
+    horiz = T
+    )
   
   on.exit(par(par(no.readonly = T))) # Show legend with names
 }
